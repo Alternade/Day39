e8c1f99c8b5bcc79908bf0cdf3d84dd7635c7377
Alternade <aptsahu2@gmail.com>
2021-09-09 00:39:50 +0530

    Day39 Problems

---
+++ ArrayQ3.js
@@ -0,0 +1,19 @@
+function Tri(arr) {
+    let found = false;
+    for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+        for (let k = j + 1; k < arr.length; k++) {
+            if (arr[i] + arr[j] + arr[k] === 0)
+            {
+                console.log("True");
+            }
+        }
+    }
+    if(found === false) {
+        console.log("False");
+    }
+}
+}
+
+arr = [0, -1, 2, -3, 1 , 2, 3];
+Tri(arr);
---
+++ arrayq2.js
@@ -0,0 +1,18 @@
+function primeFactors(n){
+    var factors = [], 
+        divisor = 2;
+  
+    while(n>2){
+      if(n % divisor == 0){
+         factors.push(divisor); 
+         n = n / divisor;
+      }
+      else{
+        divisor++;
+      }     
+    }
+    return factors;
+  }
+  
+  var a = primeFactors(559);
+  console.log(a);
